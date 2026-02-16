4d31516bf04b27cb1e9771cd74cc8192c54cfdf8
RH1114 <rauleagleblack@gmail.com>
2026-02-15 19:33:57 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,27 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "strategic-cache-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
